2428174136c2614f688b8897a682e0a716c6bc11
danielpfernandes <daniel.paivafernandes@gmail.com>
2018-05-23 09:55:56 -0300

    Resolução até exercício 13.5

--- fj11-contas/src/br/com/caelum/contas/modelo/Conta.java
+++ fj11-contas/src/br/com/caelum/contas/modelo/Conta.java
@@ -6,31 +6,48 @@
 package br.com.caelum.contas.modelo;
 
 public abstract class Conta {
-	
+
 	protected double saldo;
 	private String titular;
 	private int numero;
 	private String agencia;
+
+	@Override
+	public String toString() {
+		return "[titular=" + titular + ", numero=" + numero + ", agencia=" + agencia + "]";
+	}
 	
+	@Override
+	public boolean equals (Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		
+		Conta outraConta = (Conta) obj;
+		
+		return this.numero == outraConta.numero &&
+		this.agencia.equals(outraConta.agencia);
+	}
+
 	/**
 	 * Metodo que incrementa o saldo.
+	 * 
 	 * @param valor
 	 */
-	
-	public void deposita (double valor) {
+
+	public void deposita(double valor) {
 		if (valor < 0) {
-			throw new IllegalArgumentException("você tentou depositar +"
-					+ "um valor negativo");
+			throw new IllegalArgumentException("você tentou depositar +" + "um valor negativo");
 		} else {
 			this.saldo += valor;
 		}
 	}
-	
+
 	public void saca(double valor) {
 		this.saldo -= valor;
 	}
-	
-	public void transfere (double valor, Conta conta) {
+
+	public void transfere(double valor, Conta conta) {
 		this.saca(valor);
 		conta.deposita(valor);
 	}
@@ -62,9 +79,8 @@ public abstract class Conta {
 	public void setAgencia(String agencia) {
 		this.agencia = agencia;
 	}
-	
+
 	public String getTipo() {
 		return "Conta";
 	}
-	
 }
